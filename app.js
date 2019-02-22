2ceb604eec2f2ff6119bc142c76b5b3e049df280
NnnNnnNnnC <skychenhao1996@qq.com>
2019-02-23 01:04:49 +0800

    123

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-const parse=require('parse-server').ParseServer;
+/*
+启动服务
+ */
+const { exec } = require('child_process');
+const path=require('path');
+let database_conf_path=path.join(__dirname,'database_confi');
+//const bat = spawn(`cd ${DashBard_conf_path}`);
+exec('mongodb-runner start',(err)=>{
+    console.log(err,1)
+});
+exec(`f:&&cd ${database_conf_path}&&parse-server parseConf.json`,(err)=>{
+    console.log(err,2)
+});
+exec(`f:&&cd ${database_conf_path}&&parse-dashboard --config dashboard.json`,(err)=>{
+    console.log(err,3)
+});
+
