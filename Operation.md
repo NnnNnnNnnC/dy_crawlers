6fc71b38e81feb90bbb73f968ed162ec98998673
NnnNnnNnnC <skychenhao1996@qq.com>
2019-02-22 10:57:50 -0800

    123

--- Operation.md
+++ Operation.md
@@ -3,7 +3,9 @@
 *   以 ubuntu为例，进行安装指南
     1.  mongodb
         *   ``sudo apt install mongodb nodejs npm``;
+    2.  更新到最新版本的node.js
+        *   ``  npm install n -g``
     2.  mongodb-runner,parse-server;
-        *   ``npm install mongodb-runner parse-server -g``;
+        *   ``npm install mongodb-runner parse-server parse-dashboard -g``;
         
         
--- app.js
+++ app.js
@@ -3,15 +3,30 @@
  */
 const { exec } = require('child_process');
 const path=require('path');
-let database_conf_path=path.join(__dirname,'database_confi');
+let database_conf_path=path.join(__dirname,'parseServer_config');
 //const bat = spawn(`cd ${DashBard_conf_path}`);
-exec('mongodb-runner start',(err)=>{
-    console.log(err,1)
-});
-exec(`f:&&cd ${database_conf_path}&&parse-server parseConf.json`,(err)=>{
-    console.log(err,2)
-});
-exec(`f:&&cd ${database_conf_path}&&parse-dashboard --config dashboard.json`,(err)=>{
-    console.log(err,3)
-});
+exec('mongodb-runner start',(error, stdout, stderr) => {
+    if (error) {
+      console.error(`执行出错: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+  });
+exec(`cd ${database_conf_path}&&parse-server parseConf.json`,(error, stdout, stderr) => {
+    if (error) {
+      console.error(`执行出错: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+  });
+exec(`cd ${database_conf_path}&&parse-dashboard --config dashboard.json`,(error, stdout, stderr) => {
+    if (error) {
+      console.error(`执行出错: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+  });
 
