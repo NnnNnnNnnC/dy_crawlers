41798211dde0feaa35700629a3332fb1d9be0d16
NnnNnnNnnC <skychenhao1996@qq.com>
2019-02-23 18:51:18 +0800

    '123123'

--- douyu_lottery_room/process_emit_lottery.js
+++ douyu_lottery_room/process_emit_lottery.js
@@ -18,44 +18,77 @@ const lottery_url_file = path.join(__dirname, 'Directory_traversal_room.js');
 const lottery_url = cp.fork(lottery_url_file);
 class process_emit_lottery {
     constructor() {
+        //抽奖进行对比的目标数组
         this.arr = [];
+        //抽奖缓存数组
+        this.arr_1 = [];
+        //执行计数器
+        this.start = true;
     }
     static main() {
         const fn = new process_emit_lottery();
         fn.get_lottery_url();
     }
     get_lottery_url() {
-        lottery_url.on('message', (msg) => __awaiter(this, void 0, void 0, function* () {
-            let data = douyu_room_message_1.douyu_room_message.room_lottery_message(msg);
-            for (let i = 0; i < data.length; i++) {
-                this.save_lottery_message(this.arr).then().catch();
-            }
-        }));
+        return __awaiter(this, void 0, void 0, function* () {
+            lottery_url.on('message', (msg) => __awaiter(this, void 0, void 0, function* () {
+                if (msg === false) {
+                    this.start = false;
+                    if (this.arr.length != 0) {
+                        this.arr = [].concat(this.arr_1);
+                    }
+                    this.arr_1.splice(0, this.arr_1.length);
+                }
+                else {
+                    let data = douyu_room_message_1.douyu_room_message.room_lottery_message(msg);
+                    for (let i in data) {
+                        let x = [].concat(this.lottery_activity_id(this.arr, yield data[i]));
+                        yield this.save_lottery_message(x);
+                    }
+                }
+            }));
+        });
     }
     //保存抽奖房间数据
     save_lottery_message(data) {
         return __awaiter(this, void 0, void 0, function* () {
-            const lottery_room = Parse.Object.extend("lottery_room");
-            const lottery = new lottery_room();
-            for (let i in data) {
-                for (let k in data[i]) {
-                    lottery.set(k, data[i][k]);
+            if (data.length !== 0) {
+                const lottery_room = Parse.Object.extend("lottery_room");
+                const lottery = new lottery_room();
+                for (let i in data) {
+                    for (let k in data[i]) {
+                        lottery.set(k, data[i][k]);
+                    }
                 }
+                return lottery.save();
             }
-            return lottery.save();
         });
     }
     //抽奖策略 抽奖号码对比移除同样的抽奖room_list activity_id 返回目标数组不存在的
     lottery_activity_id(index, refer) {
-        let a = index.filter(msg => {
-            if (msg.activity_id === refer.activity_id) {
-                return false;
+        let k = [];
+        if (index.length != 0) {
+            //移除当前数组中相同的元素
+            let g = [].concat(index.filter(msg => {
+                if (msg.activity_id === refer.activity_id) {
+                    return false;
+                }
+                else {
+                    return true;
+                }
+            }));
+            if (g.length === index.length) {
+                this.arr_1.push(refer);
+                k.push(refer);
             }
-            else {
-                return true;
+        }
+        else {
+            if (this.start === true) {
+                this.arr_1.push(refer);
+                k.push(refer);
             }
-        });
-        a.push(refer);
+        }
+        return k;
     }
 }
 process_emit_lottery.main();
--- douyu_lottery_room/process_emit_lottery.ts
+++ douyu_lottery_room/process_emit_lottery.ts
@@ -1,4 +1,3 @@
-import {array_util} from '../util/array_util';
 import {douyu_room_message} from './douyu_room_message'
 const cp=require('child_process');
 const  path=require('path');
@@ -11,43 +10,73 @@ const lottery_url=cp.fork(lottery_url_file);
 
 class process_emit_lottery{
     private arr: any[];
+    private arr_1: any[];
+    private start: boolean;
     constructor(){
-        this.arr=[]
+        //抽奖进行对比的目标数组
+        this.arr=[];
+        //抽奖缓存数组
+        this.arr_1=[];
+        //执行计数器
+        this.start=true;
     }
     public static  main(){
         const fn=new process_emit_lottery();
         fn.get_lottery_url();
     }
-    private  get_lottery_url(){
+    private  async get_lottery_url(){
         lottery_url.on('message',async (msg)=>{
-            let data=douyu_room_message.room_lottery_message(msg);
-            for(let i =0;i<data.length;i++){
-
-                this.save_lottery_message(this.arr).then().catch()
+            if (msg===false){
+                this.start=false;
+                if (this.arr.length!=0){
+                    this.arr=[].concat(this.arr_1);
+                }
+                this.arr_1.splice(0,this.arr_1.length);
+            }else{
+                let data=douyu_room_message.room_lottery_message(msg);
+                for (let i in data){
+                    let x=[].concat(this.lottery_activity_id(this.arr,await data[i]));
+                    await this.save_lottery_message(x)
+                }
             }
         })
     }
     //保存抽奖房间数据
     private async save_lottery_message(data:Array<object|any>){
-        const lottery_room = Parse.Object.extend("lottery_room");
-        const lottery = new lottery_room();
-        for (let i in data){
-            for (let k in data[i]){
-                lottery.set(k,data[i][k])
+        if (data.length!==0){
+            const lottery_room = Parse.Object.extend("lottery_room");
+            const lottery = new lottery_room();
+            for (let i in data){
+                for (let k in data[i]){
+                    lottery.set(k,data[i][k])
+                }
             }
+            return lottery.save()
         }
-        return lottery.save()
     }
     //抽奖策略 抽奖号码对比移除同样的抽奖room_list activity_id 返回目标数组不存在的
     private lottery_activity_id(index:Array<object|any>,refer:object|any){
-        let a=index.filter(msg=>{
-            if(msg.activity_id===refer.activity_id){
-                return false
-            }else{
-                return true
+        let k=[];
+        if (index.length!=0){
+            //移除当前数组中相同的元素
+            let g=[].concat(index.filter(msg=>{
+                if (msg.activity_id===refer.activity_id){
+                    return false
+                }else{
+                    return true
+                }
+            }));
+            if (g.length===index.length){
+                this.arr_1.push(refer);
+                k.push(refer)
             }
-        });
-        a.push(refer)
+        }else{
+            if (this.start===true){
+                this.arr_1.push(refer);
+                k.push(refer)
+            }
+        }
+        return k
     }
 }
 process_emit_lottery.main();
